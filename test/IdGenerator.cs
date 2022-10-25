75c352504bf7b1eea5581933e50b9e4e315bce67
Jasper Chen <jasperschen@gmail.com>
2022-10-25 12:43:37 -0400

    Fix bug with ID

---
+++ test/IdGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace test
+{
+
+	public class IdGenerator
+	{
+		private int _id = -1 ;
+		public int Id { get { _id++; return _id; } }
+		public IdGenerator()
+		{
+		}
+
+	}
+
+}
